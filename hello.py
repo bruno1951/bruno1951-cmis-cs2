ba5fa88049097a209c84a118eff1906d1af64b04
cmis <cmis@cmis-hs07.(none)>
2016-01-27 11:14:07 +0700

    Having fun with steven

---
+++ hello.py
@@ -0,0 +1,29 @@
+
+print "pew"
+print "pew"
+print "pew"
+print "pew"
+print "pew"
+print "pew"
+print "Hi"
+print "Hi"
+print "Hi"
+print "Hi"
+print "Hi"
+print "Hi"
+print "Hi"
+print "Hi"
+print False
+print True
+print 3
+print 4
+print 5
+print 52413
+print True, False
+print 0.0
+print 0.7, 0.23, 0.13
+print "Hello, world", "Python", "beans"
+print ("Nathon", "Kieffer", "Bruno")
+print ("2112310.2.30.103.302.02302..0.0.2.20.302.0.20.22.02.00120.0", "dont give up you coward", "I hate stefene", "I dislkie kimchi", " im hungwy", " you a hhahahahahahashlsdafhsl;fhasdl;kf sifhsklfds hogks hfklhg [sfkf nlksdh khgsalkhf sahd f0arihohg si[ogh br[ieahg psiah gough eifag haihg ]iaheg i]ah goadhgsoiaish sdu awre yi ]ipdh pid h op]sfgpos]fjg ]apsdo hjdpifgh pgh]e hj]ps")
+print ("Steven can go choke on a sausage and die and I hope someone will eat his corspes, and puke it out, and eat it again cause that how much steven should go and die and hopefully he does not know what im saying now, He thinks im five, I think he's a friend :D ")
+
