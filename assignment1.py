8a53e30836bad92132408800bc83649f1e2db01d
cmis <cmis@cmis-hs07.(none)>
2016-02-01 09:56:51 +0700

    F

---
+++ assignment1.py
@@ -0,0 +1,29 @@
+Myname = " Bruno Dusit Haour" 
+ 
+Myageinyears =  17.75 
+
+Heightincm = 171 
+
+Sideofsquareincm =  100  
+
+Sideofrectangleincm = 150
+
+Heightofrectanglecm = 100
+
+Monthsinyear = 12
+
+Mymonth = Myageinyears * Monthsinyear 
+
+Averagelifeexpectancyinyears = 82.57
+
+Yearsleft = Averagelifeexpectancyinyears - Myageinyears
+
+Heightinft = Heightincm / 30.48 
+
+Heightinfrance = 175.6 
+
+Heightdifference = Heightinfrance - Heightincm 
+print Heightdifference 
+
+
+
