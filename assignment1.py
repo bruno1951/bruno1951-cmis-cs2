8c85c3ea2e122879c23cdb84c4dde869c850d18f
cmis <cmis@cmis-hs07.(none)>
2016-02-02 11:53:23 +0700

    m

--- assignment1.py
+++ assignment1.py
@@ -1,29 +1,43 @@
-Myname = " Bruno Dusit Haour" 
+Myname = " Bruno Dusit Haour"  #This will indicate my name
  
-Myageinyears =  17.75 
+Myageinyears =  17.75 #This will indicate how old I am in years
 
-Heightincm = 171 
+Heightinm = 1.71  # This will indicate how tall I am in meters 
+ 
+Sideofsquareincm =  100  # This will indicate how long the side of the square is
 
-Sideofsquareincm =  100  
+Sideofrectangleincm = 150 # This indicates the side length of the rectangle
 
-Sideofrectangleincm = 150
+Heightofrectanglecm = 100 # This indicates the height of the rectangle 
 
-Heightofrectanglecm = 100
+Monthsinyear = 12 # This will help the process of finding how old I am in months
 
-Monthsinyear = 12
+Mymonth = Myageinyears * Monthsinyear #This will show how old I am in months, which 213
 
-Mymonth = Myageinyears * Monthsinyear 
+Averagelifeexpectancyinyears = 82.57 # This help show how many years left I will have to live 
 
-Averagelifeexpectancyinyears = 82.57
+Yearsleft = Averagelifeexpectancyinyears - Myageinyears # This will show how many years left I have in my life, which is 64.82
 
-Yearsleft = Averagelifeexpectancyinyears - Myageinyears
+Heightinft = Heightinm *100  / 30.48 # This will put my height from meters to centimeters, to feet
 
-Heightinft = Heightincm / 30.48 
+Heightinfrance = 1.756 #This will help show how tall I am compared to the rest of my country
 
-Heightinfrance = 175.6 
+Heightdifferencem = Heightinfrance - Heightinm # This will show how tall I am compared to the average in France, which is 0.046 meters shorter
 
-Heightdifference = Heightinfrance - Heightincm 
-print Heightdifference 
+Areaofsquarecm= Sideofsquareincm * Sideofsquareincm # This will help me find the area of the square
 
+Volumeofsquare = Areaofsquarecm ** 3  # This will find the volume of the square
+
+HalfofVolumeofsquare = Volumeofsquare / 2 #instructions indicate find half of the volume, so 1 divide it by 2 
+
+Areaofrect = Sideofrectangleincm * Heightofrectanglecm * 1/9 # This will find the area of the rectangle 
+
+print "My name is " + str(Myname) + " One of my main nationalities is French, where the average height is " + str(Heightinfrance) + "meters, which is " + str(Heightdifferencem) + "meters above my height, which means my height is " + str(Heightinm) + "meters. Actually, I am pretty small for a " + str(Myageinyears) + "years old, Well, hope you enjoyed listening to this lecture. " # This will take all of the codes I wrote and put it into a paragraph format
+
+print " My age in months is " ,Mymonth, "months. Statistically, the average age expectancy in France is " ,Averagelifeexpectancyinyears, " years, meaning that I have at least " , Yearsleft, "years left in my life, which I'm thankful for. Did you know that if a square had sides that were " ,Sideofsquareincm, " cm long, then that means, that the area will be " , Areaofsquarecm , ". That was completely unrelated." # This will take all of the codes I wrote and put it into a paragraph format
+
+wrinkingface = ";)" # To indicate the representative icon so then I can multiply it by 10000
+
+print wrinkingface * 10000 # So then I will have a 10000 winking faces 
 
 
