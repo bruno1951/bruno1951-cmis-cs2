87905a4c55f3ad023fbf07297a2d859561d16d4f
cmis <cmis@cmis-hs07.(none)>
2016-02-09 14:06:39 +0700

    F

--- functions.py
+++ functions.py
@@ -1,6 +1,38 @@
-def all (a ,b ,c ,d ,e ,f)
-	return a + b + c + d + e + f
+def add(a, b): #creating formula so then the cpu can add
+	return a+b
 
-print all (50 + 50 + 650 + 8645 + 245 + 5468) 
+def sub(a, b): #creating formula so then the cpu can sub
+	return a-b 
 
+def mul(a,b): #creating formula so then the cpu can multiply
+	return a*b
+
+def div(a,b): #creating formula so then the cpu can divide
+	return a/b 
+
+def hours_from_seconds(a): #creating formula so then the cpu can find the number of seconds in an hour
+	return a/3600 
+print hours_from_seconds(86400)
+
+import math 
+
+def circle_area(a):
+	return math.pi * (a**2)
+
+print circle_area(5)
+
+
+def sphere_volume(a):
+	return 1.33333333333 * math.pi * (a**3) 
+
+print sphere_volume(5)
+
+def avg_volume (a):
+	return 
+
+
+ 
+
+
+	
 
