5d9b19937673f2c654a04b54ff6ef5adb333596a
cmis <cmis@cmis-hs07.(none)>
2016-02-15 14:35:32 +0700

    F

---
+++ play.py
@@ -0,0 +1,5 @@
+x = raw_input(" Take your wand out:")
+y = raw_input(" You're a wizard youngone: ")
+z = raw_input(" Please come with me and become a wizard: ")
+p = raw_input(" No, I am not a liar: " )
+print str(x) + " and repeat " + str(y) + "I have never seen such potential in such a young boy" + str(z) + "Young one, you will be taken care of very well, theres nothing to be afraid of, I promise" + str(p) + "Come, it is time" 
