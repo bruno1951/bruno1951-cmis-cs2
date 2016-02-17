eae3a3fcfd88b7196558077593b3cd9bce9dbcc4
cmis <cmis@cmis-hs07.(none)>
2016-02-17 09:57:17 +0700

    F

---
+++ simple.py
@@ -0,0 +1,23 @@
+print """What the heck"""
+
+x = raw_input("Type a number")
+y = raw_input("Type a number")
+z = int(x) + int(y) 
+b= Bruno 
+
+output = """
+Hey {}!
+Did you know:
+{} + {} =  {}
+""".format(b, x, y, z)
+
+print output 
+def main():
+name = raw_input("Whats your name")
+x= raw_input("Type a number")
+y= raw_input("Type  a number")
+
+z = add(int(x)), int(y))
+
+out = output(name, x,y,z)
+
