411b686d29f055d3e537d71d3951f22f29bbd9a3
cmis <cmis@cmis-hs07.(none)>
2016-02-18 12:00:06 +0700

    F

--- functions.py
+++ functions.py
@@ -1,33 +1,34 @@
 def add(a, b): #creating formula so then the cpu can add
 	return a+b
-print add(3, 4)
+
+
 def sub(a, b): #creating formula so then the cpu can sub
 	return a-b
-print add(5, 3) 
+
 
 def mul(a,b): #creating formula so then the cpu can multiply
 	return a*b
-print mul(4,4)
+
 
 def div(a,b): #creating formula so then the cpu can divide
 	return a/b 
-print div(2, 3)
+
 def hours_from_seconds(a): #creating formula so then the cpu can find the number of seconds in an hour
 	return a/3600 
-print hours_from_seconds(86400)
+
 
 import math 
 
 def circle_area(a): #defining the process that is required to find an normal area of a circle
 	return math.pi * (a**2)
 
-print circle_area(5)
+
 
 
 def sphere_volume(a): # Defining the equations that is required to to find out the volume of a sphere
 	return 1.33333333333 * math.pi * (a**3) 
 
-print sphere_volume(5)
+
 
 def avg_volume(a,b): #this will help find the average volume of those two areas 
 	c= a/2
@@ -37,7 +38,7 @@ def avg_volume(a,b): #this will help find the average volume of those two areas
 	return (y + x) /2
 
 
-print avg_volume(10,20) 
+
 
 
 def area(a,b,c) : # This will help find the area of this triangle 
@@ -45,12 +46,12 @@ def area(a,b,c) : # This will help find the area of this triangle
 	s= (a+b+c) / 2 
 	return (s*(s - a) * (s - b) * (s - c)) ** 0.5
  
-print area(1,2,2.5)
+
 
 def right_align(a): # this will right align the message 
 	
 	return (80-len(a))*(" ") + a
-print right_align( "Hello" )  
+
 
 
 
@@ -58,43 +59,50 @@ print right_align( "Hello" )
 def center (a): # This will centralize the message in the middle 
 	
 	return (40-len(a))*(" ") + a
-print center("Hallo") 
+
 
 
 def msg_box(a): #This will 
 	return "+" + ((len(a)+4)*"-") + "+" + "\n" + "|" + (2*" ") + (a)+ (2*" ") + "|" + "\n" + "+" + ((len(a)+4)*"-") + "+"  
-print msg_box("Hello")
-print msg_box(" I finally got this done " ) 
- 
 
 
-print add(3, 4)
-print add(5, 3) 
-print mul(4,4)
-print div(2, 3)
-print hours_from_seconds(86400)
-print circle_area(5)
-print sphere_volume(5)
-print avg_volume(10,20) 
-print area(1,2,2.5)
-print right_align( "Hello" )  
-print center("Hallo") 
-print msg_box("Hello")
-print msg_box(" I finally got this done " ) 
  
-first = add(3, 4) 
-second = add(5, 3) 
-third = mul(4,4)
-fourth = div(2, 3)
-fifth = hours_from_seconds(86400)
-sixth = circle_area(5)
-seventh = sphere_volume(5)
-eighth = avg_volume(10,20) 
-ninth = area(1,2,2.5)
-tenth = right_align( "Hello" )  
-eleven = center("Hallo") 
-twelve = msg_box("Hello")
-thirdteen = msg_box(" I finally got this done " ) 
  
-thewholething = first + second + third + fourth + fifth + sixth + seventh + eighth + ninth + tenth + eleven + twelve + thirdteen
-print thewholething
+add1 = add (5, 1) 
+add2 = add(7, 6)
+ 
+sub1 = sub(4,4)
+sub2 = sub(9,5)
+
+mul1 = (6, 5)
+mul2 = (8, 2)
+
+div1 = (5,6)
+div2 = (6,7)
+
+hours_from_seconds1 = (21,600)
+hours_from_seconds2 = (32400)
+
+circle_area1 = (4)
+circle_area2 = (6)
+
+sphere_volume1 = (7)
+sphere_volume2 = (8)
+
+avg_volume1 = (15, 25) 
+avg_volume2 = (35, 80)
+
+area1 = (2, 3 , 3.5) 
+area2 = (5, 6, 6.5)
+
+right_align1 = ("Herro")
+right_align2 = (" This is hard ")
+
+center1 = ("This better be centralized")
+center2 = (" Bomin is old for her grade")
+
+msg_box1 = (" Steven smells " )
+msg_box2 = (" My partners are meanys" )
+
+
+print msg_box(str(add1))
