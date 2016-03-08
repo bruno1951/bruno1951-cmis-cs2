3319faaea05e0545d2b29c19ed3e6327b6a694bf
cmis <cmis@cmis-hs07.(none)>
2016-03-08 09:56:36 +0700

    F

---
+++ happyandknowit.py
@@ -0,0 +1,29 @@
+
+def clapYourHands():
+    print "clap clap"
+
+def stompYourFeet():
+    print "stomp stomp"
+
+def main():
+	youreHappy = raw_input (""" Are you happy, type True or False """) == "True"
+	youKnowIt = raw_input (""" IF you know it, type True or False  """) == "True" 
+
+	if youreHappy and youKnowIt: #boolean expression 
+		clapYourHands()
+		stompYourFeet()
+
+	print "Good bye"
+main()
+
+x = 26
+y = 25
+z = 0
+
+if x < y :
+	print "Okay"
+	print "Yes"
+	z = x + y
+print "Done"
+print z
+
