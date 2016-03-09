ba1062fe20390f62cd0d7fb042e4dad2d472cc5b
cmis <cmis@cmis-hs07.(none)>
2016-03-09 11:53:25 +0700

    F

--- happyandknowit.py
+++ happyandknowit.py
@@ -27,3 +27,27 @@ if x < y :
 print "Done"
 print z
 
+
+
+
+
+
+import random 
+
+def wrestle(playerStrength, enemyStrength, playerAttackValue):
+    
+	targetValue = random.random()
+
+	playerAttackResult = playerStrength + abs(playerAttackValue - targetValue)    
+
+	enemyAttackResult =  enemyStrength + targetValue
+ 
+	if playerAttackResult > enemyAttackResult:
+		return True
+	else:
+        	return False
+
+print wrestle(0 , 0 ,0 )
+
+
+
