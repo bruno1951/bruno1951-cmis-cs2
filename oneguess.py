8a82f1f358f3c71db49e6a914df5e5b727f2990c
cmis <cmis@cmis-hs07.(none)>
2016-03-15 09:55:35 +0700

    F

--- oneguess.py
+++ oneguess.py
@@ -1,8 +1,25 @@
 import random
 
-def random_number
-minimum = raw_input("Type in the mininum number")
-maximum = raw_input("Type in the maximum number")
+def main(): 
+	minimum = raw_input("Type in the minimum number")
+	maximum = raw_input("Type in the maximum number")
+
+	output = """ I'm thinking of a number between {} and {} """.format(minimum, maximum)
+	print output
+
+
+	number = random.randint(minimum, maximum)
+	your_number = raw_input("What do you think was the number?")
+
+
+
+
+	
+
+
+
+main()
+
 
 
 
@@ -16,75 +33,5 @@ maximum = raw_input("Type in the maximum number")
 #That's under by 2.
 
 
-
-def getAttackValue():
- 
-    number = int(raw_input("Type attack value (0-99): "))
-    if number > 99 or number < 0:
-        number = random.randint(0, 99)
-    attackValue = float(number)/100
-    return attackValue    
-
-
-def wrestle(playerStrength, enemyStrength, playerAttackValue):
-
-    targetValue = random.random()
-    playerAttackResult = playerStrength + abs(playerAttackValue - targetValue)    
-    enemyAttackResult =  enemyStrength + targetValue
-    if playerAttackResult > enemyAttackResult:
-        return True
-    else:
-        return False
-
-
-
-
-
-def resultTemplate(playerStrength, enemyStrength, result):
-
-    if result == True:
-        msg = "wins"
-    else:
-        msg = "loses"
-    return """
-Player strength: {}
-Enemy strength: {}
-Player {}!
-""".format(playerStrength, enemyStrength, msg)
-
-
-
-
-
-
-
-def main():
-
-
-    playerFood = raw_input("What do you want to eat? ")
-    enemy1Food = raw_input("What does enemy 1 eat? ")
-    enemy2Food = raw_input("What does enemy 2 eat? ")
-
-    playerStrength = eat(playerFood)
-    enemy1Strength = eat(enemy1Food)
-    enemy2Strength = eat(enemy2Food)
-
-    playerAttackValue1 = getAttackValue()
-    result1 = wrestle(playerStrength, enemy1Strength, playerAttackValue1)
-
-    playerAttackValue2 = getAttackValue()   
-    result2 = wrestle(playerStrength, enemy2Strength, playerAttackValue2)
-    
-    output = """
-You ate {}.
-Enemy 1 ate {}.
-Enemy 2 ate {}.
-""".format(playerFood, enemy1Food, enemy2Food)
-    output += resultTemplate(playerStrength, enemy1Strength, result1)
-    output += resultTemplate(playerStrength, enemy2Strength, result2)
-
-    print output
-
-main()
 
 
