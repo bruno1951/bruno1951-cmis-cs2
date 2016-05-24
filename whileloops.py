59ff5496df9faf901f8511a628ea35a6f2ac9c4d
cmis <cmis@cmis-hs08.(none)>
2016-05-24 09:55:39 +0700

    F

---
+++ whileloops.py
@@ -0,0 +1,37 @@
+x = 10
+while x <= 0:
+	print x
+	x -= 1
+
+
+
+
+def whiles(x):
+
+	while x > 0:
+		print x
+		x -= 1
+		
+
+
+def counter(x):
+	while x < 0:
+		print x 
+		x += 1
+
+	while x > 0:
+		print x 
+		x -= 1
+
+
+def addup(x): 
+
+
+
+def main():
+	whiles(10)
+	counter(15)
+
+main()
+
+
