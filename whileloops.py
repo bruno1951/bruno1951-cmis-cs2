cc231f01057f3a33ad9e23d2aad50c2d503185d2
cmis <cmis@cmis-hs19.(none)>
2016-05-26 14:37:38 +0700

    F

--- whileloops.py
+++ whileloops.py
@@ -25,13 +25,34 @@ def counter(x):
 
 
 def addup(x): 
+	pass
 
+def countFrom2(x,y):	
+	while x < y:
+		print x
+		x += 1
+	while x > y:
+		print x 
+		x -= 1
+
+def sumofOdds(x): 
+	while x < 0:
+		print x
+		x += 1
+	
+
+	while x > 1:
+		print x
+		x -= 1
+	
+		
 
 
 def main():
 	whiles(10)
 	counter(15)
-
+	countFrom2(63, 43)
+	sumofOdds(54)
 main()
 
 
