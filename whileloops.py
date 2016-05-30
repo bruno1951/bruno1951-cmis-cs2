303c1c1da6b515b19b3304e660777af2c4a739e0
cmis <cmis@cmis-hs07.(none)>
2016-05-30 09:58:32 +0700

    F

--- whileloops.py
+++ whileloops.py
@@ -1,13 +1,13 @@
-x = 10
-while x <= 0:
-	print x
-	x -= 1
+def first(x):
+	while x <= 0:
+		print x
+		x -= 1
 
 
 
 
 def whiles(x):
-
+	
 	while x > 0:
 		print x
 		x -= 1
@@ -15,6 +15,7 @@ def whiles(x):
 
 
 def counter(x):
+	
 	while x < 0:
 		print x 
 		x += 1
@@ -27,7 +28,8 @@ def counter(x):
 def addup(x): 
 	pass
 
-def countFrom2(x,y):	
+def countFrom2(x,y):
+		
 	while x < y:
 		print x
 		x += 1
@@ -35,24 +37,47 @@ def countFrom2(x,y):
 		print x 
 		x -= 1
 
-def sumofOdds(x): 
-	while x < 0:
-		print x
-		x += 1
-	
+def sumofOdds(x):
+	total = 0 
+	if x > 0:
+		while x > 0:
+			if x % 2 == 1:
+				total += x
 
-	while x > 1:
-		print x
-		x -= 1
+			x -= 1
+	elif x < 0:	
+		while x < 0:
+			if x % 2 == 1:
+				total += x
+			x += 1
+	
+	return total
+	
 	
 		
 
+def grid(w, h):
+	out = ""
+	while h > 0:
+		h = w
+		while w > 0:
+			out += "."
+			w -= 1
+		out += "\n"
+		h -= 1
+	return out
+
+		
+	
+
+
+
 
 def main():
-	whiles(10)
-	counter(15)
-	countFrom2(63, 43)
-	sumofOdds(54)
+
+	print sumofOdds(6)
+	print grid(60, 20)
+
 main()
 
 
